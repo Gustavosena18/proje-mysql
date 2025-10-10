6ae0145dec1b162e6a25d362b594562186b8ca45
Gustavo Silva <gustavosena0971@gmail.com>
2025-10-10 13:51:15 -0300

    função listar alunos

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 from conexao import conectar
 
 def inserir_aluno(nome_aluno, idade_aluno):
-    conexao, cursor = conectar()
+    conexao,cursor = conectar()
     if conexao:
         try:
             cursor.execute(
@@ -14,3 +14,18 @@ def inserir_aluno(nome_aluno, idade_aluno):
         finally:
             cursor.close()
             conexao.close()
+
+def listar_alunos():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM alunos ORDER BY id"
+            )
+            return cursor.fetchall()
+        except Exception as erro:
+            print(f"Erro a listar os alunos: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
