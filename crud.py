292ced39d2d44db52454344336103a56f6572a5f
Gustavo Silva <gustavosena0971@gmail.com>
2025-10-10 14:42:55 -0300

    função  deletar aluno

--- crud.py
+++ crud.py
@@ -45,3 +45,22 @@ def atualizar_idade(id_aluno, nova_idade):
             cursor.close()
             conexao.close()
 
+def deletar_aluno(id_aluno):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "DELETE FROM alunos WHERE id = %s",
+                (id_aluno,)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao tentar excluir aluno {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+
+            
+                       
+
